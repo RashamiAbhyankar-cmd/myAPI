52a1e2f8f50494a3da25c92cb14d0b84c0f8884f
SN Abhyankar <72180299+RashamiAbhyankar-cmd@users.noreply.github.com>
2026-03-06 22:25:30 +0800

    app.py

--- app.py
+++ app.py
@@ -112,5 +112,6 @@ def search_recipesbyid():
         })
 
 if __name__ == '__main__':
-    #app.run(debug=True, port=5000)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
+    
+
